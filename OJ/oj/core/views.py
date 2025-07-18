dcfabfb9257eabcdee181ff54607b4f7fa5fc507
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2025-07-18 19:12:17 +0530

    CORE App updated

--- OJ/oj/core/views.py
+++ OJ/oj/core/views.py
@@ -9,42 +9,8 @@ def home(request):
     return render(request, "home.html")
 
 
-def login(request):
-    """
-    Render the login page of the OJ application.
-    """
-    return render(request, "login.html")
-
-
-def signup(request):
-    """
-    Render the signup page of the OJ application.
-    """
-    return render(request, "signup.html")
-
-
 def all_problems(request):
     """
     Render the list of problems available in the OJ application.
     """
     return render(request, "problems.html")
-
-
-def leaderboard(request):
-    """
-    Render the leaderboard page of the OJ application.
-    """
-    return render(request, "leaderboard.html")
-
-
-def contest(request):
-    """
-    Render the contest page of the OJ application.
-    """
-    return render(request, "contests.html")
-
-def dashboard(request):
-    """
-    Render the dashboard page of the OJ application.
-    """
-    return render(request, "dashboard.html")
