880a1574e794a4b02a1e101711a6f5f0c4c7ea03
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2025-07-28 22:16:11 +0530

    Migrated to Postgress

--- oj/oj/settings.py
+++ oj/oj/settings.py
@@ -11,10 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-import dotenv
-
-dotenv.load_dotenv()
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,23 +88,23 @@ WSGI_APPLICATION = "oj.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'your_db_name',
-#         'USER': 'your_user',
-#         'PASSWORD': 'your_password',
-#         'HOST': 'localhost',  # or the IP/hostname of your DB
-#         'PORT': '5432',        # default PostgreSQL port
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("POSTGRESS_DB_NAME"),
+        'USER': os.getenv("POSTGRESS_USER_NAME"),
+        'PASSWORD': os.getenv("POSTGRESS_PASSWORD"),
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
