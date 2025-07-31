80fca3cb033a72699bb0fcdc9bcfc72d86fd793f
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2025-08-01 00:44:53 +0530

    Update urls.py

--- oj/contests/urls.py
+++ oj/contests/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import contests_view, leaderboard_view
+from .views import leaderboard_view #,contests_view
 
 app_name = "contests"
 
 
 urlpatterns = [
-    path("contest/", contests_view, name="contest"),
+    # path("contest/", contests_view, name="contest"),
     path("leaderboard/", leaderboard_view, name="leaderboard"),
 ]
