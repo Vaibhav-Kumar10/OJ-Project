5653dc702efa881c9d457ba0f52286666b0eb44f
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2025-08-08 02:46:34 +0530

    Updated the leaderboard

--- oj/contests/views.py
+++ oj/contests/views.py
@@ -1,6 +1,8 @@
 from django.db.models import Count, ExpressionWrapper, F, FloatField
 from django.shortcuts import render
 from .models import UserScore
+from core.models import Problem
+from .models import UserScore
 
 
 def leaderboard_view(request):
@@ -9,13 +11,13 @@ def leaderboard_view(request):
         .annotate(
             total_submissions=Count("user__submission", distinct=True),
             accuracy=ExpressionWrapper(
-                100.0 * F("problems_solved") / (F("user__submission") + 0.001),
+                100.0 * F("problems_solved") / (F("total_submissions") + 0.001),
                 output_field=FloatField(),
             ),
         )
         .order_by("-score", "-problems_solved")
+        .distinct("user")  # Ensures each user appears only once
     )
-
     context = {"scores": scores}
     return render(request, "contests/leaderboard.html", context=context)
 
